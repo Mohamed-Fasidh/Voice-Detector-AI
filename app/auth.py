dcc5cdbe43eb4cd2fb8c086afe0d5d58e5dcf579
Mohamed-Fasidh <21uad051@kamarajengg.edu.in>
2026-02-04 21:01:02 +0530

    Intial Commit

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,9 @@
+import os
 from fastapi import Header, HTTPException
-from app.config import API_KEY
+
+API_KEY = os.getenv("API_KEY")
 
 def validate_api_key(x_api_key: str = Header(...)):
     if x_api_key != API_KEY:
         raise HTTPException(status_code=401, detail="Invalid API key")
+    return x_api_key
